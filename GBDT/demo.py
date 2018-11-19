19caa238f7b472bbf9e19a985033ec96251d21d0
redhand <30518857+QuickyFinger@users.noreply.github.com>
2018-11-19 18:32:19 +0800

    Update demo.py

--- GBDT/demo.py
+++ GBDT/demo.py
@@ -1,3 +1,12 @@
 from GBDT import GBDT
+import pandas as pd
 
 def demo():
+    dataset = pd.read_csv('../training_data.csv')
+    test_data = pd.read_csv('../test_data.csv')
+    model = GBDT()
+    model.fit(dataset)
+    result = model.predict_value(test_data)
+
+if __name__ == '__main__':
+    demo()
