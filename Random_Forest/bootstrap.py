b0a8e7181a23a81bf46e579cc0693e2ca88d39b4
redhand <30518857+QuickyFinger@users.noreply.github.com>
2018-11-23 19:25:17 +0800

    Update bootstrap.py

--- Random_Forest/bootstrap.py
+++ Random_Forest/bootstrap.py
@@ -1,11 +1,12 @@
 import random
 import math
+import pandas as pd
 
-def bootstrap(data):
+def bootstrap(x_data, y_data):
+    data = pd.concat([x_data, y_data], axis = 1)
     n, m = data.shape
     feature_names = random.sample(data.columns.values[:-1], int(math.sqrt(m - 1)))
     feature_names.append(data.columns.values[-1])
     rows = [random.randint(0, n-1) for _ in range(n)]
     traindata = data.iloc[rows][feature_names]
-    return train_data, feature_names
-    
+    return train_data[:-1], train_data[-1]   
